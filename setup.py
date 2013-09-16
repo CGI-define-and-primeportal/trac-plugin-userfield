4f1e52e0715d0481947cfc9a55d7a76243f9b2fa
Ronny Östman <ronny.ostman@cgi.com>
2013-09-16 14:58:38 +0000

    Merge [18585] to 4.10.x branch for #3602
    git-svn-id: file:///mnt/ramdisk/d4-svn/branches/releases/4.10.x@18592 484df14f-75d2-4b3f-8ba0-490e05069a01

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013 CGI IT UK Ltd
+
+from setuptools import setup
+
+setup(
+    name='UserFieldPlugin',
+    version=0.1,
+    description='Adds a user custom field automatically populated by groups',
+    author="Ian Clark",
+    author_email="ian.clark@cgi.com",
+    license='BSD',
+    url='http://define.primeportal.com/',
+    packages=['userfield'],
+    package_data={
+        'userfield': [
+            'htdocs/css/*.css',
+            'htdocs/js/*.js',
+            'templates/*',
+        ]
+    },
+    install_requires = ['LogicaOrderTracker',],
+    entry_points={
+        'trac.plugins': [
+            'userfield.filter = userfield.filter',
+        ]
+    },
+)
---
+++ userfield/htdocs/js/customfield-admin.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  var $type = $("#type"),
+      $wrapper = $("#user-groups"),
+      $select = $("#select-user-groups"),
+      $radios = $("input[name='all_or_selection']");
+
+  $select.select2();
+  var $select2 = $("#s2id_select-user-groups");
+
+  // Show the "Included Groups" row only when type "user" set
+  $type.change(function() { $wrapper.toggle($type.val() == "user"); });
+  $type.change();
+
+  // If there's an initial set value of "all", hide the select2
+  if($radios.filter(":checked").val() == "all") $select2.hide();
+
+  // When we change the radio button's value, slide the select2 up/down
+  $($radios).change(function() {
+    if($(this).val() == "selection") {
+      $select2.slideDown();
+    }
+    else {
+      $select2.slideUp();
+    }
+  });
+})

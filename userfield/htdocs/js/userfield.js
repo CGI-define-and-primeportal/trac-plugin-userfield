d9b97a281a94b50048a356c15ec4267d66b7306e
Ian Clark <ian.clark@cgi.com>
2013-11-25 16:46:51 +0000

    Fixes #3891 - Makes user list respect autocomplete's "shown groups"
    * The JS now checks to see if the group is a valid one before pushing the results into the select2 options.
      * This means that if a group was manually selected to be included in a drop down, but it is no longer available / specified that it shouldn't be made available, then it'll be hidden
    
    git-svn-id: file:///mnt/ramdisk/d4-svn/trunk@21739 484df14f-75d2-4b3f-8ba0-490e05069a01

--- userfield/htdocs/js/userfield.js
+++ userfield/htdocs/js/userfield.js
@@ -57,7 +57,6 @@ $(document).ready(function($) {
 
     // Process clicking on remote group
     $select.on("select2-selecting", function(e) {
-      console.log("selecting");
       event_option_selected(e, $select);
     });
   }
@@ -123,7 +122,10 @@ $(document).ready(function($) {
         break;
       }
       else {
-        opts.data.results.push(get_group_data(groupName));
+        var group = window.userGroups[groupName];
+        if(group) {
+          opts.data.results.push(get_group_data(group));
+        }
       }
     }
 
@@ -167,15 +169,12 @@ $(document).ready(function($) {
    * cache the results for reuse
    */
   var optgroupCache = {};
-  function get_group_data(groupName) {
-    var group = window.userGroups[groupName];
-    if(group) {
-      if(group.members) {
-        return data_optgroup(groupName, group);
-      }
-      else {
-        return data_remote(groupName, group.label);
-      }
+  function get_group_data(group) {
+    if(group.members) {
+      return data_optgroup(groupName, group);
+    }
+    else {
+      return data_remote(groupName, group.label);
     }
   }
 
